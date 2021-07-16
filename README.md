eb78dcb434f5f46b0569815e4e877d237e74d1c4
ViRybalkin <kinjeks@gmail.com>
2021-07-16 12:23:48 +0300

    Add features Add functionality of closing modal and add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Modal Window - приложение по монипуляции c Dom
+
+![preview](https://github.com/ViRybalkin/ModalWindow/blob/main/Preview.jpg)
+
+### При написании проекта использованы технологии
+
+- HTML
+- CSS
+- JavaScript
+
+##### Чтобы посмотреть проект перейдите по [ссылке](https://virybalkin.github.io/ModalWindow/).
--- script.js
+++ script.js
@@ -9,6 +9,21 @@ const showModal = () => {
   overlay.classList.remove('hidden');
 };
 
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 btn.forEach(el => {
   el.addEventListener('click', showModal);
+});
+
+cross.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
