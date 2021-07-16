88b979e151b8faa1030a7b22c07f464472575395
ViRybalkin <kinjeks@gmail.com>
2021-07-16 12:06:20 +0300

    add functionality of showing modal

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+const btn = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const cross = document.querySelector('.close-modal');
+
+const showModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+btn.forEach(el => {
+  el.addEventListener('click', showModal);
+});
